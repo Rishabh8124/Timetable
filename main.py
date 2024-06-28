ffa5d8448b3e85f7ffa4180d1dd8e210821b5a2d
Rishabh8124 <rishabh8124@kgpian.iitkgp.ac.in>
2024-06-28 21:18:37 +0530

    Defaults file replace

--- main.py
+++ main.py
@@ -5,6 +5,94 @@ from Academic_Year import *
 l = os.listdir()
 if "Academic_years" not in l:
     os.mkdir("Academic_years")
+    
+defaults = {
+    "class_list": {
+        "1": [
+            "A",
+            "B",
+            "C",
+            "D"
+        ],
+        "2": [
+            "A",
+            "B",
+            "C",
+            "D"
+        ],
+        "3": [
+            "A",
+            "B",
+            "C",
+            "D"
+        ],
+        "4": [
+            "A",
+            "B",
+            "C",
+            "D"
+        ],
+        "5": [
+            "A",
+            "B",
+            "C",
+            "D"
+        ],
+        "6": [
+            "A",
+            "B",
+            "C",
+            "D"
+        ],
+        "7": [
+            "A",
+            "B",
+            "C",
+            "D"
+        ],
+        "8": [
+            "A",
+            "B",
+            "C",
+            "D"
+        ],
+        "9": [
+            "A",
+            "B",
+            "C",
+            "D"
+        ],
+        "10": [
+            "A",
+            "B",
+            "C",
+            "D"
+        ],
+        "11": [
+            "A",
+            "B",
+            "C"
+        ],
+        "12": [
+            "A",
+            "B",
+            "C"
+        ]
+    },
+    "timetable": [
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", ""]
+    ]
+}
+
+if 'defaults.json' not in l:
+    with open('defaults.json', 'w') as file:
+        json_object = json.dumps(defaults, indent=4)
+        file.write(json_object)
 
 root = Tk()
 
